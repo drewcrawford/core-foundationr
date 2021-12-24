7a28df3d866f58ba3a22f114a6cda010f5a160b2
Drew Crawford <drew@sealedabstract.com>
2021-12-23 19:48:47 -0600

    [StrongCell] implement std::fmt::Display

--- src/cell.rs
+++ src/cell.rs
@@ -32,6 +32,14 @@ impl<T: CFType> std::fmt::Debug for StrongCell<T> {
         f.write_fmt(format_args!("StrongCell<{}>({:?})",stringify!(T),self.0))
     }
 }
+impl <T: std::fmt::Display + CFType> std::fmt::Display for StrongCell<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        //I think this is fine because of the precedent in Drop.
+        // todo: Maybe we should convert the inner pointer to NonNull...
+        let as_ref = unsafe {&*self.0};
+        f.write_fmt(format_args!("{}",as_ref))
+    }
+}
 
 extern "C" {
     fn CFRelease(type_ref: *const c_void);
